39aa877c092eb4a77283e0f32f358875bdb2b53c
himanshuprasad200 <himanshuprasad468@gmail.com>
2026-03-29 15:26:46 +0530

    payments methods

--- src/actions/projectAction.jsx
+++ src/actions/projectAction.jsx
@@ -24,7 +24,7 @@ import {
   UPDATE_PROJECT_SUCCESS,
 } from "../constants/projectConstant";
 
-const API_URL = 'https://backend-1-mp9a.onrender.com';
+const API_URL = 'http://localhost:4050';
 
 export const getProject =
   (keyword = "", currentPage = 1, price = [0, 90000], category, ratings = 0) =>
